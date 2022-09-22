425cf3c9231830aaad0528a8fc71b70af74ec484
Hoang <aswx12@gmail.com>
2022-09-22 11:44:38 +0200

    Revert "Updates"
    This reverts commit 25cb144a9618db47beb4b0fa3d61c15439f1b251.

---
+++ Detached/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float crouchHeight;
+    [SerializeField] private bool active;
+    CapsuleCollider playerCol;
+    float originHeight;
+
+    [Header("Movement")]
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float crouchSpeed;
+    float walkSpeed;
+    [SerializeField] private Transform camTransform;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCD;
+    [SerializeField] private float airMultiplier;
+    [SerializeField] private float groundDrag;
+    bool readyToJump;
+
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheckTransform;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask groundMask;
+
+    private bool isGrounded = false;
+
+    Vector3 moveDir;
+
+    float horizontalInput;
+    float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerCol = GetComponent<CapsuleCollider>();
+        originHeight = playerCol.height;
+        ResetJump();
+        walkSpeed = movementSpeed;
+    }
+
+    private void Update()
+    {
+        if (active)
+        {
+            GroundCheck();
+            MyInput();
+            Movement();
+            Jump();
+            Sprint();
+            Crouch();
+
+            SpeedControl();
+
+            //Debug.Log(movementSpeed);
+
+            if (isGrounded)
+                rb.drag = groundDrag;
+            else
+                rb.drag = 0;
+        }
+    }
+
+    void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (flatVel.magnitude > movementSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * movementSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    void Movement()
+    {
+        moveDir = new Vector3(  horizontalInput,0, verticalInput);
+        moveDir = Quaternion.AngleAxis(camTransform.rotation.eulerAngles.y, Vector3.up)*moveDir;
+
+        if (isGrounded)
+            rb.AddForce(moveDir.normalized * movementSpeed * 10f, ForceMode.Force);
+        else if (!isGrounded)
+            rb.AddForce(moveDir.normalized * movementSpeed * 10f * airMultiplier, ForceMode.Force);
+
+        //transform.position += moveDir * movementSpeed * Time.deltaTime;
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+            Cursor.lockState = CursorLockMode.Locked;
+        else
+            Cursor.lockState = CursorLockMode.None; 
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(groundCheckTransform.position, groundCheckRadius, groundMask);
+    }
+
+    private void Jump()
+    {
+        if (isGrounded && Input.GetButton("Jump") && readyToJump)
+        {
+            readyToJump = false;
+
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+
+            Invoke(nameof(ResetJump), jumpCD); //Hold jump
+        }
+    }
+
+    void Sprint()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = runSpeed;
+        }
+        else
+            movementSpeed = walkSpeed;
+    }
+
+    void Crouch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            playerCol.height = crouchHeight;
+            movementSpeed = crouchSpeed;
+        }
+        else
+        {
+            playerCol.height = originHeight;
+        }
+    }
+
+    void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawSphere(groundCheckTransform.position, groundCheckRadius);
+    }
+
+
+}
