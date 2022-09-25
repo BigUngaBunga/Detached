bcb0f8ff2ce7d4b61c52b26c750f4fa4aeca6c38
FredrikAndreasson <fredrik.andreasson@yandex.com>
2022-09-26 01:54:40 +0200

    Better movement for the camera in the editor

--- Level Editor/Assets/Movement.cs
+++ Level Editor/Assets/Movement.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] private float movementSpeed = 0.05f;
+    [SerializeField] private float movementSpeed = 1.0f;
+    [SerializeField] private GameObject mover;
 
 
+    Vector2 turn;
+    Vector3 deltaMove;
 
     void Update()
     {
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal") * movementSpeed, 0, Input.GetAxis("Vertical") * movementSpeed);
-
-        if (move != Vector3.zero)
+        if (Input.GetMouseButton(1))
         {
-            gameObject.transform.position += move;
+            turn.x += Input.GetAxis("Mouse X");
+            turn.y += Input.GetAxis("Mouse Y");
         }
+
+        mover.transform.localRotation = Quaternion.Euler(0, turn.x, 0);
+        transform.localRotation = Quaternion.Euler(-turn.y, 0, 0);
+
+        deltaMove = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * movementSpeed * Time.deltaTime;
+        mover.transform.Translate(deltaMove);
+
+        if (Input.GetKey("space"))
+        {
+            mover.transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("left ctrl"))
+        {
+            mover.transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+        }
+
     }
 }
