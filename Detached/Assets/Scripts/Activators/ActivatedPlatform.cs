84d02bfb37d8a6d80fd75d6a576406393e1d1917
BigUngaBunga <truls.rockstrom.ystad.nu@gmail.com>
2022-09-28 14:23:35 +0200

    Battery sockets, small buttons and upgrades
    Implemented rough battery sockets and small buttons, they only require collisions not actual interactions. The alpha values on platforms have been improved. Elevators now go up and down i a more logical manner and objects don't fly of when going down.

--- Detached/Assets/Scripts/Activators/ActivatedPlatform.cs
+++ Detached/Assets/Scripts/Activators/ActivatedPlatform.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 public class ActivatedPlatform : Activator
 {
     [Header("Platform fields")]
-    [SerializeField] Material activeMaterial;
-    [SerializeField] Material inactiveMaterial;
+    [Range(0.05f, 1f)]
+    [SerializeField] float inactiveAlpha = 0.33f;
+    [Range(0.05f, 0.33f)]
+    [SerializeField] float minimumAlpha = 0.1f;
+    private Color color;
     private BoxCollider collider;
     private MeshRenderer meshRenderer;
+    private float PercentageActive => (float)ActiveConnections / (float)TotalConnections;
 
     private void Awake()
     {
         meshRenderer = GetComponent<MeshRenderer>();
+        color = meshRenderer.material.color;
         collider = GetComponent<BoxCollider>();
     }
 
@@ -18,13 +23,26 @@ public class ActivatedPlatform : Activator
     {
         base.Activate();
         collider.enabled = true;
-        meshRenderer.material = activeMaterial;
+        UpdateMaterial();
     }
 
     protected override void Deactivate()
     {
         base.Deactivate();
         collider.enabled = false;
-        meshRenderer.material = inactiveMaterial;
+        UpdateMaterial();
+    }
+
+
+    private void UpdateMaterial()
+    {
+        if (IsActivated)
+            color.a = 1f;
+        else if(activationRequirement.Equals(ActivationRequirement.All))
+            color.a = Mathf.Max(PercentageActive / 2f, minimumAlpha);
+        else
+            color.a = inactiveAlpha;
+
+        meshRenderer.material.color = color;
     }
 }
--- Detached/Assets/Scripts/Activators/Magnet Scripts/MagnetTargeting.cs
+++ Detached/Assets/Scripts/Activators/Magnet Scripts/MagnetTargeting.cs
@@ -6,13 +6,17 @@ public class MagnetTargeting : MonoBehaviour
 {
     private List<GameObject> presentPlayers = new List<GameObject>();
     private bool PlayerIsPresent => presentPlayers.Count > 0;
+    private float heightTargeting;
+    [SerializeField] private float angleWidth;
+    [SerializeField] private float inputSpeed;
 
     public void Update()
     {
         if (PlayerIsPresent)
         {
+            AdjustHeight();
             transform.LookAt(presentPlayers[0].transform);
-            transform.eulerAngles += new Vector3(90, 0, 0);
+            transform.eulerAngles += new Vector3(90 + heightTargeting, 0, 0);
         }
     }
 
@@ -26,5 +30,11 @@ public class MagnetTargeting : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
             presentPlayers.Remove(other.gameObject);
+    }
+
+    private void AdjustHeight()
+    {
+        float input = Input.GetAxis("Mouse ScrollWheel") * -1 * inputSpeed;
+        heightTargeting = Mathf.Clamp(heightTargeting + input, -angleWidth, angleWidth);
     }
 }
