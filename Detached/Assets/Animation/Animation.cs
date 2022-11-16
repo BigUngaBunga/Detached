a17b3d8f9fd99d4743078a71140003095ce2dc9e
David Hoang <62556785+Zereis@users.noreply.github.com>
2022-11-16 16:43:18 +0100

    Sync animation

--- Detached/Assets/Animation/Animation.cs
+++ Detached/Assets/Animation/Animation.cs
@@ -35,25 +35,30 @@ public class Animation : NetworkBehaviour
                 else if (Input.GetKey(KeyCode.S))
                 {
                     animator.SetTrigger("CarryWalk");
+                    networkAnimator.SetTrigger("CarryWalk");
                 }
                 else if (Input.GetKey(KeyCode.A))
                 {
                     animator.SetTrigger("CarryWalk");
+                    networkAnimator.SetTrigger("CarryWalk");
                 }
                 else if (Input.GetKey(KeyCode.D))
                 {
                     animator.SetTrigger("CarryWalk");
+                    networkAnimator.SetTrigger("CarryWalk");
                 }
                 else if (Input.GetKeyDown(KeyCode.F))
                 {
                     if (animator.GetBool("isCarrying") == true)
                     {
                         animator.SetBool("isCarrying", false);
+                 
                     }
                 }
                 else
                 {
                     animator.SetTrigger("CarryIdle");
+                    networkAnimator.SetTrigger("CarryIdle");
                 }
             }
 
@@ -63,18 +68,22 @@ public class Animation : NetworkBehaviour
                 if (Input.GetKey(KeyCode.W) && detach.LeftArm.active == false && detach.RightArm.active == false)
                 {
                     animator.SetTrigger("Crawling");
+                    networkAnimator.SetTrigger("Crawling");
                 }
                 else if (Input.GetKey(KeyCode.S))
                 {
                     animator.SetTrigger("Crawling");
+                    networkAnimator.SetTrigger("Crawling");
                 }
                 else if (Input.GetKey(KeyCode.A))
                 {
                     animator.SetTrigger("Crawling");
+                    networkAnimator.SetTrigger("Crawling");
                 }
                 else if (Input.GetKey(KeyCode.D))
                 {
                     animator.SetTrigger("Crawling");
+                    networkAnimator.SetTrigger("Crawling");
                 }
             }
             else if (detach.LeftLeg.active == false)
@@ -82,18 +91,23 @@ public class Animation : NetworkBehaviour
                 if (Input.GetKey(KeyCode.W))
                 {
                     animator.SetTrigger("Right Jump");
+                    networkAnimator.SetTrigger("Right Jump");
+
                 }
                 else if (Input.GetKey(KeyCode.S))
                 {
                     animator.SetTrigger("Right Jump");
+                    networkAnimator.SetTrigger("Right Jump");
                 }
                 else if (Input.GetKey(KeyCode.A))
                 {
                     animator.SetTrigger("Right Jump");
+                    networkAnimator.SetTrigger("Right Jump");
                 }
                 else if (Input.GetKey(KeyCode.D))
                 {
                     animator.SetTrigger("Right Jump");
+                    networkAnimator.SetTrigger("Right Jump");
                 }
             }
             else if(detach.RightLeg.active == false)
@@ -101,18 +115,22 @@ public class Animation : NetworkBehaviour
                 if (Input.GetKey(KeyCode.W))
                 {
                     animator.SetTrigger("Left Jump");
+                    networkAnimator.SetTrigger("Left Jump");
                 }
                 else if (Input.GetKey(KeyCode.S))
                 {
                     animator.SetTrigger("Left Jump");
+                    networkAnimator.SetTrigger("Left Jump");
                 }
                 else if (Input.GetKey(KeyCode.A))
                 {
                     animator.SetTrigger("Left Jump");
+                    networkAnimator.SetTrigger("Left Jump");
                 }
                 else if (Input.GetKey(KeyCode.D))
                 {
                     animator.SetTrigger("Left Jump");
+                    networkAnimator.SetTrigger("Left Jump");
                 }
             }
 
@@ -120,22 +138,27 @@ public class Animation : NetworkBehaviour
             if (Input.GetKey(KeyCode.W) && detach.LeftLeg.active == true && detach.RightLeg.active == true)
             {
                 animator.SetTrigger("Walking");
+                networkAnimator.SetTrigger("Walking");
             }
             else if (Input.GetKey(KeyCode.S) && detach.LeftLeg.active == true && detach.RightLeg.active == true)
             {
                 animator.SetTrigger("Walking");
+                networkAnimator.SetTrigger("Walking");
             }
             else if (Input.GetKey(KeyCode.A) && detach.LeftLeg.active == true && detach.RightLeg.active == true)
             {
                 animator.SetTrigger("Walking");
+                networkAnimator.SetTrigger("Walking");
             }
             else if (Input.GetKey(KeyCode.D) && detach.LeftLeg.active == true && detach.RightLeg.active == true)
             {
                 animator.SetTrigger("Walking");
+                networkAnimator.SetTrigger("Walking");
             }
             else if (Input.GetKey(KeyCode.Space))
             {
                 animator.SetTrigger("Jump");
+                networkAnimator.SetTrigger("Jump");
             }
             else if (Input.GetKeyDown(KeyCode.E))
             {
@@ -144,26 +167,31 @@ public class Animation : NetworkBehaviour
                     animator.SetBool("isCarrying", false);
                 }
                 animator.SetTrigger("Carry");
+                networkAnimator.SetTrigger("Carry");
                 animator.SetBool("isCarrying", true);
             }
             else if (!Input.anyKey)
             {
                 animator.SetTrigger("Idle");
-
+                networkAnimator.SetTrigger("Idle");
             }
 
             if (Input.GetKeyDown(KeyCode.Z))
             {
 
                 animator.SetTrigger("Facepalm");
+                networkAnimator.SetTrigger("Facepalm");
             }
             else if (Input.GetKeyDown(KeyCode.X))
             {
                 animator.SetTrigger("Wave");
+                networkAnimator.SetTrigger("Wave");
             }
             else if (Input.GetKeyDown(KeyCode.C))
             {
                 animator.SetTrigger("Mock");
+
+                networkAnimator.SetTrigger("Mock");
             }
             else if (Input.GetKeyDown(KeyCode.V))
             {
