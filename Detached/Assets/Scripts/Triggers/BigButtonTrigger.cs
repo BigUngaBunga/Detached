d80bf8b764fde2ed0e0aec110cf69c09f157115d
BigUngaBunga <truls.rockstrom.ystad.nu@gmail.com>
2022-11-30 19:29:01 +0100

    Biggus buttonius de fixus

--- Detached/Assets/Scripts/Triggers/BigButtonTrigger.cs
+++ Detached/Assets/Scripts/Triggers/BigButtonTrigger.cs
@@ -15,10 +15,12 @@ public class BigButtonTrigger : Trigger, IInteractable
         get => triggeringObjects; 
         set {
             triggeringObjects = value;
-            if (triggeringObjects == 1)
+            if (triggeringObjects >= 1)
                 IsTriggered = true;
-            else if (triggeringObjects == 0)
+            else if (triggeringObjects <= 0)
                 IsTriggered = false;
+            if (triggeringObjects < 0)
+                triggeringObjects = 0;
         } 
     }
     [SerializeField] private int triggeringObjects;
@@ -96,8 +98,6 @@ public class BigButtonTrigger : Trigger, IInteractable
     private void RemoveTrigger()
     {
         TriggeringObjects--;
-        if (TriggeringObjects < 0)
-            TriggeringObjects = 0;
     }
 
     private void CheckObjectsOnButton()
