15e1f7de1d866dd2bc52e06df04d2fabbd105dd3
FredrikAndreasson <fredrik.andreasson@yandex.com>
2022-12-06 14:39:57 +0100

    Fixed the Lines that were causing build errors in the scripts

--- Detached/Assets/Scripts/Interactables/InteractableManager.cs
+++ Detached/Assets/Scripts/Interactables/InteractableManager.cs
@@ -2,7 +2,7 @@ using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress; // this was causing build errors.
 
 public class InteractableManager : NetworkBehaviour
 {
