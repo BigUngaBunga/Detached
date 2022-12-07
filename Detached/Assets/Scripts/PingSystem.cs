d7b327e80320b0c2e6f44d192b9804b31638b1eb
Simon W <31246612+Sizzox@users.noreply.github.com>
2022-12-07 15:36:22 +0100

    Added Pings to the Game

--- Detached/Assets/Scripts/PingSystem.cs
+++ Detached/Assets/Scripts/PingSystem.cs
@@ -7,23 +7,36 @@ public class PingSystem : MonoBehaviour
     [Header("GameObject")]
 
     [SerializeField] GameObject pingPoint;
+    GameObject resetGO;
     int timer = 0;
     private Transform objectHit;
     private string nameObject;
     private bool hitObject;
+    RaycastHit hit;
+    Ray ray;
+    [SerializeField] Camera camera;
+    Vector2 center = new Vector2(Screen.width / 2, Screen.height / 2);
+
     // Start is called before the first frame update
     void Start()
     {
+        camera = GameObject.Find("Camera").GetComponent<Camera>();
+        resetGO = pingPoint;
         pingPoint.active = false;
     }
 
+
+    void PlaySoundOnTrigger()
+    {
+        SFXManager.PlayOneShot(SFXManager.JumpSound, SFXManager.SFXVolume, pingPoint.transform.position);
+    }
     // Update is called once per frame
     void Update()
     {
         timer++;
         if(timer >= 400)
         {
-            pingPoint.active = false;
+            ResetPing();
             timer = 0;
         }
         if (Input.GetMouseButtonDown(0)) //Replace jump with apropriate key
@@ -32,8 +45,7 @@ public class PingSystem : MonoBehaviour
             Ping();
         }
 
-        RaycastHit hit;
-        Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+         ray = camera.ScreenPointToRay(center);
 
         hitObject = false;
         if (Physics.Raycast(ray, out hit))
@@ -42,13 +54,25 @@ public class PingSystem : MonoBehaviour
             objectHit = hit.transform;
             nameObject = hit.transform.gameObject.name;
         }
-
+            
+    }
+    
+    void ResetPing()
+    {
+        pingPoint = resetGO;
+        pingPoint.transform.position = this.transform.position;
+        pingPoint.transform.parent = this.transform;
+        pingPoint.active = false;
     }
 
     void Ping()
     {
-        pingPoint.active = true;
-        pingPoint.transform.position = objectHit.transform.position;
+        ResetPing();
+            pingPoint.active = true;
+            pingPoint.transform.position = new Vector3(objectHit.transform.position.x, objectHit.transform.position.y + 3f, objectHit.transform.position.z);
+        //pingPoint.transform.rotation = Quaternion.identity;
+        pingPoint.transform.parent = null;
 
+        PlaySoundOnTrigger();
     }
 }
