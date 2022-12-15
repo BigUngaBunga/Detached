1146606337e21139022667a6f0185772eb0a72be
BigUngaBunga <truls.rockstrom.ystad.nu@gmail.com>
2022-12-15 12:07:21 +0100

    Indicator improvement
    Improved the selection indicator and resolutions changing

--- Detached/Assets/Scripts/Interactables/LegoColourRandomizer.cs
+++ Detached/Assets/Scripts/Interactables/LegoColourRandomizer.cs
@@ -1,9 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-using Mono.CecilX;
-using System.Linq;
 
 public class LegoColourRandomizer : NetworkBehaviour
 {
