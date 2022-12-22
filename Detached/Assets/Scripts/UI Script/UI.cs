cc438cb2fe48b3f289c30a03a34aeea58d159b72
OlofN <Olof_norrlind@hotmail.com>
2022-12-22 05:01:09 +0100

    Fixed closing on option and controls

--- Detached/Assets/Scripts/UI Script/UI.cs
+++ Detached/Assets/Scripts/UI Script/UI.cs
@@ -7,6 +7,8 @@ public class UI : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject pauseUI;
+    public GameObject optionUIMenu;
+    public GameObject controlUIMenu;
     public GameObject mainMenu;
     public bool gameIsPaused;
     void Start()
@@ -19,6 +21,8 @@ public class UI : MonoBehaviour
         gameIsPaused = false;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        optionUIMenu.SetActive(false);
+        controlUIMenu.SetActive(false);
     }
     public void FreezeGame()
     {
